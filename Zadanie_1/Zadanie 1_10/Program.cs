5e5ae1b8fcf623f26ac0e2f3f4ccc890b4fb5548
Daniil <124627774+SISDRUG@users.noreply.github.com>
2024-03-04 11:09:05 +0300

    Add 10-11 + sun

---
+++ Zadanie_1/Zadanie 1_10/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("zadayte chislo");
+        string x = Console.ReadLine();
+        int max = 0;
+        int min = 10;
+        int indigit;
+
+        foreach (char digit in x)
+        {
+            indigit = Convert.ToInt32(digit.ToString());
+            if (max < indigit)
+            {
+                max = indigit;
+            }
+            if (min > indigit)
+            {
+                min = indigit;
+            }
+        }
+
+        Console.WriteLine($"max {max}");
+        Console.WriteLine($"min {min}");
+    }
+}
---
+++ Zadanie_1/Zadanie 1_11/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("zadayte chislo");
+        string x = Console.ReadLine();
+        string res="";
+
+        int indigit;
+
+        foreach (char digit in x)
+        {
+            indigit = Convert.ToInt32(digit.ToString());
+            if (indigit % 2 != 0)
+            {
+                res += digit;
+            }
+
+        }
+        Console.WriteLine("chislo bez chetnix");
+        Console.WriteLine(res);
+    }
+}
