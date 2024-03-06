2a3b2a0a265a7a088e01fa2ac323c697dcff4a68
Daniil <124627774+SISDRUG@users.noreply.github.com>
2024-03-06 14:03:25 +0300

    Add files via upload

---
+++ Zadanie_3/Zadanie 3_4/Program.cs
@@ -0,0 +1,105 @@
+﻿internal class Program
+{
+
+    public class StringMatr
+    {
+        string[,] stmatr;
+        int row;
+        int col;
+
+        public StringMatr()
+        {
+
+        }
+
+        public void size(int row, int col)
+        {
+            this.stmatr = new string[row, col];
+            this.row = row;
+            this.col = col;
+        }
+
+        public StringMatr(StringMatr matr)
+        {
+            this.stmatr = matr.stmatr;
+            this.row = matr.row;
+            this.col = matr.col;
+        }
+
+
+
+        public StringMatr(int row, int col, string stroke)
+        {
+            this.row = row;
+            this.col = col;
+            this.stmatr = new string[row,col];
+            string[] cells = stroke.Split(';');
+            int c = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    this.stmatr[i, j] = cells[c] ;
+                    c++;
+                }
+            }
+            
+        }
+
+        public StringMatr(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+            this.stmatr = new string[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine($"Input cell [{i},{j}]");
+                    this.stmatr[i, j] = Console.ReadLine();
+
+                }
+            }
+
+        }
+
+        public static StringMatr operator + (StringMatr f , StringMatr s)
+        {
+            StringMatr r = new();
+            r.size(f.row,f.col);
+            for (int i = 0; i < f.row; i++)
+            {
+                for (int j = 0; j < f.col ; j++)
+                {
+                    r.stmatr[i,j] = f.stmatr[i,j] + s.stmatr[i,j];
+                }
+            }
+            return r;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < this.row; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < this.col; j++)
+                {
+                    Console.Write($"{stmatr[i,j]} ");
+                }
+            }
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        StringMatr stringMatr = new(3, 3,"a;a;a;b;b;b;c;c;c");
+        //StringMatr str = new(3, 3);
+        StringMatr f = new(3, 3, "a;a;a;b;b;b;c;c;c");
+        StringMatr s = new(3, 3, "b;b;b;c;c;c;a;a;a");
+        StringMatr t = f + s;
+        t.Print();
+        (f + s).Print();
+        //str.Print();
+        //stringMatr.Print();
+    }
+}
---
+++ Zadanie_3/Zadanie 3_5/Program.cs
@@ -0,0 +1,153 @@
+﻿internal class Program
+{
+    public class Matr
+    {
+        double[,] matr;
+        int row;
+        int col;
+
+        public Matr()
+        {
+
+        }
+
+        public double this[int row, int col]
+        {
+            get => matr[row, col];
+            set => matr[row, col] = value;
+        }
+
+        public Matr(int row,int col)
+        {
+            this.matr = new double[row,col];
+            this.col = col;
+            this.row = row;
+        }
+
+        public  Matr(Matr c)
+        {
+            this.matr = (double[,])c.matr.Clone();
+            this.row = c.row;
+            this.col = c.col;
+        }
+
+        public static Matr operator + (Matr f, Matr s)
+        {
+            Matr r = new(f);
+            for (int i = 0; i < f.row; i++)
+            {
+                for (int j = 0; j < f.col; j++)
+                {
+                    r[i, j] = f[i, j] + s[i,j];
+                }
+            }
+            return r;
+        }
+
+        public static Matr operator + (Matr f, int s)
+        {
+            Matr r = new(f);
+            for (int i = 0; i < f.row; i++)
+            {
+                for (int j = 0; j < f.col; j++)
+                {
+                    r[i, j] = f[i, j] + s;
+                }
+            }
+            return r;
+        }
+
+        public static bool operator > (Matr f, Matr s)
+        {
+            if (f.row == s.row & f.col == s.col)
+            {
+
+
+                for (int i = 0; i < f.row; i++)
+                {
+                    for (int j = 0; j < f.col; j++)
+                    {
+                        if (f[i, j] < s[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+
+
+            }
+
+            return false;
+            
+        }
+        public static bool operator <(Matr f, Matr s)
+        {
+            if (f.row == s.row & f.col == s.col)
+            {
+
+
+                for (int i = 0; i < f.row; i++)
+                {
+                    for (int j = 0; j < f.col; j++)
+                    {
+                        if (f[i, j] > s[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+
+
+            }
+
+            return false;
+
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < this.row; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < this.col; j++)
+                {
+                    Console.Write($"{this[i,j]} ");
+                }
+            }
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Matr a = new(2, 2);
+        Matr b = new(2, 2);
+        int s = 2;
+        int c = 2;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                a[i, j] = 1;
+                b[i, j] = 2;
+            }
+        }
+
+        
+
+        (a + b).Print();
+        Console.WriteLine();
+        a.Print();
+        Console.WriteLine();
+        a += b;
+        a.Print();
+        Console.WriteLine();
+
+        Console.WriteLine(a<b);
+
+    }
+}
