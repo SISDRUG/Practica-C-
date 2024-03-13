b0d6ed05e0bec15d7d6aa892b5f8169b9e090fa6
sisdrug <danik2004sabancev@gmail.com>
2024-03-13 14:41:59 +0300

    Zadanie_8 ended

---
+++ Zadanie_8/Zadanie_8_1/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Main
+{
+    struct Aeroflot
+    {
+        public string Destination;
+        public int Number;
+        public string Type;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int num = 7;
+            Aeroflot[] aeroflot = new Aeroflot[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"Input {i + 1}:");
+
+                Console.Write("Destination: ");
+                aeroflot[i].Destination = Console.ReadLine();
+
+                Console.Write("Number: ");
+                aeroflot[i].Number = int.Parse(Console.ReadLine());
+
+                Console.Write("Type: ");
+                aeroflot[i].Type = Console.ReadLine();
+            }
+
+            Array.Sort(aeroflot, (a, b) => string.Compare(a.Destination, b.Destination));
+            foreach (var flight in aeroflot)
+            {
+                Console.WriteLine($"Destination: {flight.Destination}, number: {flight.Number}, type {flight.Type}");
+            }
+            Console.WriteLine("\nSpecific type :");
+            Console.Write("input type: ");
+            string inputType = Console.ReadLine();
+            
+            bool found = false;
+            foreach (var flight in aeroflot)
+            {
+                if (flight.Type == inputType)
+                {
+                    Console.WriteLine($"Destination: {flight.Destination}, number: {flight.Number}");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Not found.");
+            }
+        }
+    }
+}
---
+++ Zadanie_8/Zadanie_8_2/Program.cs
@@ -0,0 +1,53 @@
+﻿enum Post
+{
+    Manager,      
+    Engineer,     
+    Technician    
+}
+
+class Accountant
+{
+    
+    public bool AskForBonus(Post worker, int hours)
+    {
+        const int ManagerHours = 160;
+        const int EngineerHours = 180;
+        const int TechnicianHours = 200;
+
+        switch (worker)
+        {
+            case Post.Manager:
+                return hours > ManagerHours;
+            case Post.Engineer:
+                return hours > EngineerHours;
+            case Post.Technician:
+                return hours > TechnicianHours;
+            default:
+                throw new ArgumentException("Недопустимая должность");
+        }
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Accountant accountant = new Accountant();
+
+        Console.Write("Введите должность (Manager, Engineer, Technician): ");
+        string inputPost = Console.ReadLine();
+        Console.Write("Введите отработанные часы: ");
+        int inputHours = int.Parse(Console.ReadLine());
+
+        if (Enum.TryParse(inputPost, out Post post))
+        {
+            bool bonus = accountant.AskForBonus(post, inputHours);
+            Console.WriteLine(bonus ? "Сотруднику положена премия" : "Сотруднику премия не положена");
+        }
+        else
+        {
+            Console.WriteLine("Недопустимая должность");
+        }
+    }
+}
