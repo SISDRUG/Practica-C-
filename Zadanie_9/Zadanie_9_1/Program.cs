1d93ba6422c105c5db47d9ed262e32af4a7b45c8
sisdrug <danik2004sabancev@gmail.com>
2024-03-14 12:19:58 +0300

    Zadanie 9 ended

---
+++ Zadanie_9/Zadanie_9_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Internal;
+
+internal class Program
+{
+    class cl : Ix, Iy, Iz
+    {
+        public string str = "ccc";
+
+        public void IxF0(string s)
+        {
+            str = str + s.Remove(s.Length - 2);
+            Console.WriteLine(str);
+        }
+
+        public void IxF1()
+        {
+            str = str.Insert(0, "aa");
+            str = str.Remove(str.Length - 2);
+            Console.WriteLine(str);
+        }
+
+        public void F0(string s) => Console.WriteLine((str + s).Remove(0, 2));
+        public void F1() => Console.WriteLine(str.Remove(0, 2));
+
+        void Iz.F0(string s) => Console.WriteLine('-' + (str + s));
+        void Iz.F1() => Console.WriteLine('-' + str);
+
+    }
+    interface Ix
+    {
+        void IxF0(string s);
+        void IxF1();
+    }
+
+    interface Iy
+    {
+        void F0(string s);
+        void F1();
+    }
+
+    interface Iz
+    {
+        void F0(string s);
+        void F1();
+    }
+
+    private static void Main(string[] args)
+    {
+        cl cl = new cl();
+        cl.IxF0("bbbaa");
+        cl.IxF1();
+        cl.F0("ee");
+        cl.F1();
+        (cl as Iz).F0("kk");
+        (cl as Iz).F1();
+    }
+}
---
+++ Zadanie_9/Zadanie_9_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+public class Train : IComparable<Train>
+{
+    public string Destination { get; private set; }
+    public string Number { get; private set; }
+    public DateTime Time { get; private set; }
+
+    public Train(string destination, string number, DateTime time)
+    {
+        Destination = destination;
+        Number = number;
+        Time = time;
+    }
+
+    public int CompareTo(Train other)
+    {
+        return Time.CompareTo(other.Time);
+    }
+}
+
+public class Station
+{
+    private Train[] trains;
+
+    public Station(Train[] trains)
+    {
+        this.trains = trains;
+    }
+
+    public Train this[string number]
+    {
+        get { return trains.FirstOrDefault(t => t.Number == number); }
+    }
+
+    public Train[] GetTrainsDepartingAfter(DateTime time)
+    {
+        return trains.Where(t => t.Time > time)
+                     .ToArray();
+    }
+
+    public Train[] GetTrainsToDestination(string destination)
+    {
+        return trains.Where(t => t.Destination == destination)
+                     .OrderBy(t => t.Time)
+                     .ToArray();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var trains = new Train[]
+        {
+            new Train("Minsk", "123A", new DateTime(2024, 3, 14, 14, 0, 0)),
+            new Train("Brest", "456B", new DateTime(2024, 3, 14, 16, 0, 0)),
+            new Train("Grodno", "789C", new DateTime(2024, 3, 14, 18, 0, 0))
+        };
+
+        var station = new Station(trains);
+
+        Console.WriteLine(station["123A"].Destination);
+
+        var lateTrains = station.GetTrainsDepartingAfter(new DateTime(2024, 3, 14, 17, 0, 0));
+        foreach (var train in lateTrains)
+            Console.WriteLine(train.Number);
+
+        var trainsToMinsk = station.GetTrainsToDestination("Minsk");
+        foreach (var train in trainsToMinsk)
+            Console.WriteLine(train.Number);
+    }
+}
