3f0366e9a502c93776d172b9999997c8e9d379da
sisdrug <danik2004sabancev@gmail.com>
2024-03-18 14:14:52 +0300

    Zadanie_12 add

---
+++ Zadanie_12/Zadanie_12_3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Zadanie_12_3
+{
+    public delegate string Del(string str);
+
+    class Program
+    {
+        static string Concatenation(string str)
+        {
+            string result = str + str;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        static string RemoveLastElement(string str)
+        {
+            string result = str.Remove(str.Length - 1);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        static string ToLowerCase(string str)
+        {
+            string result = str.ToLower();
+            Console.WriteLine(result);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "ZXC";
+
+            Del del = new Del(Concatenation);
+            del += RemoveLastElement;
+            del += ToLowerCase;
+
+            del(str);
+        }
+    }
+}
