98ec9153137b1cc41fbc82b9be3ef02d223549f9
sisdrug <danik2004sabancev@gmail.com>
2024-03-22 12:35:52 +0300

    Zadanie_16

---
+++ Zadanie_16/Zadanie_16_1/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("f.txt");
+        var numbers = lines.Select(int.Parse).Where(n => n % 3 == 0);
+        File.WriteAllLines("g.txt", numbers.Select(n => n.ToString()));
+    }
+}
---
+++ Zadanie_16/Zadanie_16_2/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Directory.CreateDirectory("New_folder");
+    }
+}
