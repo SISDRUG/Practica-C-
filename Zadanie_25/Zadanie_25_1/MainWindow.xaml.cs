8c3d3fdd47e9543d7ea84d4ebf61fda7b476868d
sisdrug <danik2004sabancev@gmail.com>
2024-04-03 11:09:32 +0300

    Add Zadanie_25

---
+++ Zadanie_25/Zadanie_25_1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_25_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lb1.Content = "Hello " + tb1.Text;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This program show hello name");
+        }
+    }
+}
---
+++ Zadanie_25/Zadanie_25_2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+
+namespace Zadanie_25_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PlotModel PlotModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PlotModel = new PlotModel { Title = "График функции" };
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int xS = Convert.ToInt32(xs.Text); int xE = Convert.ToInt32(xe.Text); double Step = Convert.ToDouble(step1.Text);
+            var model = new PlotModel { Title = "График Функции y = ctg(x)" };
+            var series = new FunctionSeries(x => 1.0 / Math.Tan(x), xS, xE, Step, "y=ctg(x)");
+            model.Series.Add(series);
+            PlotView1.Model = model;
+
+        }
+    }
+}
