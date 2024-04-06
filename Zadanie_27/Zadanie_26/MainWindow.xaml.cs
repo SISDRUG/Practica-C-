adb221d3eb672225f530f0c3876fb684f82ab144
sisdrug <danik2004sabancev@gmail.com>
2024-04-06 11:20:39 +0300

    Add Zadanie_27

---
+++ Zadanie_27/Zadanie_26/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zadanie_26.Interfaces;
+using Zadanie_26.Models;
+using Zadanie_26.Share;
+using Microsoft.Extensions.Logging;
+using System.Xml;
+
+namespace Zadanie_26
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IXmlWorker _worker;
+        private readonly ILogger _logger;
+        public bool IsFileOpenned = false;
+        private string _xmlFilePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _logger = LoggerFactory.Create(builder => builder
+                                            .SetMinimumLevel(LogLevel.Information))
+                                            .CreateLogger<MainWindow>();
+            _worker = new IXmlDocumentWorker(_logger);
+        }
+
+
+        public void PrintAvto(Avto avto)
+        {
+            textBlockXMlFileContent.Text = "===Avto====" + Environment.NewLine;
+            textBlockXMlFileContent.Text += avto?.ToString() ?? "AvtoNotFound";
+        }
+
+        private void textBoxDeleteCountryName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCountryName.Text))
+            {
+                PrintAvtoBlock(_worker.GetAll());
+            }
+        }
+
+        private void PrintAvtoBlock(List<Avto> avtos)
+        {
+            textBlockXMlFileContent.Text = "====Avto====" + Environment.NewLine;
+            foreach (var avto in avtos)
+            {
+                textBlockXMlFileContent.Text += avto.ToString();
+            }
+        }
+
+        private void buttonOpenFile_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            dialog.InitialDirectory = Directory.GetParent(AppContext.BaseDirectory)
+                .Parent
+                .Parent
+                .FullName;
+
+            dialog.DefaultExt = ".xml";
+            dialog.Filter = "Xml documents (.xml)|*.xml";
+            var result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                _xmlFilePath = dialog.FileName;
+                textBlockXMLPathFile.Text = _xmlFilePath;
+                _worker.Load(_xmlFilePath);
+
+                if (radioButtonTextBlock.IsChecked == true)
+                {
+                    PrintAvtoBlock(_worker.GetAll());
+                    listBoxCountries.Visibility = Visibility.Collapsed;
+                    treeViewCountries.Visibility = Visibility.Collapsed;
+                    textBlockXMlFileContent.Visibility = Visibility.Visible;
+
+                }
+                else if (radioButtonTreeView.IsChecked == true)
+                {
+                    PrintContriesTreeView(_worker.GetAll());
+                    listBoxCountries.Visibility = Visibility.Collapsed;
+                    treeViewCountries.Visibility = Visibility.Visible;
+                    textBlockXMlFileContent.Visibility = Visibility.Collapsed;
+                }
+                else if (radioButtonListBox.IsChecked == true)
+                {
+                    PrintContriesListBox(_worker.GetAll());
+                    listBoxCountries.Visibility = Visibility.Visible;
+                    treeViewCountries.Visibility = Visibility.Collapsed;
+                    textBlockXMlFileContent.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void PrintContriesTreeView(List<Avto> countries)
+        {
+            treeViewCountries.Items.Clear();
+
+            foreach (var country in countries)
+            {
+                var countryNode = new TreeViewItem();
+                countryNode.Header = country;
+
+
+                treeViewCountries.Items.Add(countryNode);
+            }
+        }
+
+        private void PrintContriesListBox(List<Avto> countries)
+        {
+            listBoxCountries.Items.Clear();
+
+            foreach (var country in countries)
+            {
+                listBoxCountries.Items.Add(country);
+            }
+        }
+
+
+        private void buttonFindCountryName_Click_1(object sender, RoutedEventArgs e)
+        {
+            var avto = _worker.FindBy(textBoxCountryName.Text);
+            PrintAvto(avto);
+        }
+
+        private void buttonDelete_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxDeleteCountryName.Text) || !string.IsNullOrWhiteSpace(textBoxDeleteCountryName.Text))
+            {
+                _worker.Delete(textBoxDeleteCountryName.Text);
+                PrintAvtoBlock(_worker.GetAll());
+            }
+        }
+
+        private void buttonAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            PrintAvtoBlock(_worker.GetAll());
+        }
+
+        private void buttonExit_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Zadanie_27/Zadanie_26/Share/IXmlDocumentWorker.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Zadanie_26.Interfaces;
+using Zadanie_26.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Zadanie_26.Share
+{
+    public class IXmlDocumentWorker:IXmlWorker
+    {
+        private readonly XmlDocument _document;
+        private readonly ILogger _logger;
+        private string _xmlFilePath;
+
+        public IXmlDocumentWorker(ILogger logger)
+        {
+            _logger = logger;
+            _document = new XmlDocument();
+        }
+
+        public void Add(Avto avto)
+        {
+            var xRoot = _document.DocumentElement;
+
+            XmlElement avtoElem = _document.CreateElement("avto");
+
+            XmlAttribute markaAttribute = _document.CreateAttribute("marka");
+            XmlText markaText = _document.CreateTextNode(avto.marka);
+            markaAttribute.AppendChild(markaText);
+
+            XmlAttribute yearElem = _document.CreateAttribute("year");
+            XmlText yearInnerText = _document.CreateTextNode(avto.year.ToString());
+            yearElem.AppendChild(yearInnerText);
+
+            avtoElem.AppendChild(yearElem);
+
+            XmlAttribute daysElem = _document.CreateAttribute("days");
+            XmlText daysInnerText = _document.CreateTextNode(avto.days.ToString());
+            daysElem.AppendChild(daysInnerText);
+
+            avtoElem.AppendChild(daysElem);
+
+            xRoot.AppendChild(avtoElem);
+            _document.Save(_xmlFilePath);
+
+        }
+
+        public void Delete(string marka)
+        {
+            var xRoot= _document.DocumentElement;
+            foreach (XmlNode xNode in xRoot)
+            {
+                if (xNode.Attributes.Count>0)
+                {
+                    var attributeName = xNode.Attributes.GetNamedItem("marka");
+                    try
+                    {
+                        var attributeNameText = attributeName?.InnerText;
+                        if (attributeNameText.Equals(marka))
+                        {
+                            xRoot.RemoveChild(xNode);
+                        }
+                    }
+                    catch (Exception ex) when (ex is XmlException || ex is NullReferenceException)
+                    {
+
+                        _logger.LogWarning(ex.Message, nameof(attributeName));
+                    }
+                }
+            }
+        }
+
+        public Avto FindBy(string marka)
+        {
+            Avto avto = null;
+            var xRoot = _document.DocumentElement;
+            foreach (XmlNode xmlNode in xRoot)
+            {
+                avto = GetAvto(xmlNode);
+                if (avto.marka.Equals(marka))
+                {
+                    return avto;
+                }
+            }
+            return avto;
+        }
+
+        public List<Avto> GetAll()
+        {
+            List<Avto> avtos = new List<Avto>();
+            var xRoot = _document.DocumentElement;
+            foreach (XmlNode node in xRoot)
+            {
+                var avto = GetAvto(node);
+                avtos.Add(avto);
+            }
+            return avtos;
+        }
+
+        public void Load(string xmlFilePath)
+        {
+            _xmlFilePath = xmlFilePath;
+            _document.Load(xmlFilePath);
+
+        }
+
+        private Avto GetAvto(XmlNode node)
+        {
+            var avto = new Avto();
+            if (node.Attributes.Count > 0)
+            {
+                var attributeName = node.Attributes.GetNamedItem("marka");
+                avto.marka = attributeName?.Value;
+            }
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                try
+                {
+                    if (childNode.Name.Equals("marka"))
+                    {
+                        avto.marka = childNode.InnerText;
+                    }
+                    if (childNode.Name.Equals("year"))
+                    {
+                        avto.year = int.Parse(childNode.InnerText);
+                    }
+                    if (childNode.Name.Equals("days"))
+                    {
+                        avto.days = int.Parse(childNode.InnerText);
+                    }
+                }
+                catch (Exception ex) when (ex is FormatException || ex is NullReferenceException)
+                {
+                    _logger.LogError(ex.Message, ex.StackTrace, nameof(childNode.InnerText));
+                }
+            }
+            return avto;
+        }
+
+    }
+}
