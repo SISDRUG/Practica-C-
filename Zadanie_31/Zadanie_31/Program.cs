f36be640652f133365090402bb53bf41ad9506cf
sisdrug <danik2004sabancev@gmail.com>
2024-04-11 12:47:05 +0300

    Add Zadanie_31-33

---
+++ Zadanie_31/Zadanie_31/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+class Program
+{
+    static void Main()
+    {
+
+        string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\SISDRUG\Desktop\Zadanie_31\Zadanie_31\Turisty.mdb;";
+
+        using (OleDbConnection connection = new OleDbConnection(connectionString))
+        {
+            connection.Open();
+
+            string selectQuery = "SELECT * FROM Туры";
+            OleDbCommand selectCommand = new OleDbCommand(selectQuery, connection);
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(selectQuery, connection);
+            DataSet ds = new DataSet();
+            if (adapter != null && ds != null)
+            {
+                adapter.Fill(ds);
+                Console.WriteLine("не пустой");
+                // Другие операции с данными...
+            }
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                Console.WriteLine("не пустой");
+            }
+
+            OleDbDataReader reader = selectCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Название"]);
+            }
+
+            /*
+            Console.WriteLine(ds.Tables["Table"].Rows[0].ItemArray);
+            DataRow userRowToDelete = ds.Tables["Table"].Rows[0];
+            Console.WriteLine(string.Join("\n", userRowToDelete[1]));
+
+            if (userRowToDelete != null)
+            {
+                userRowToDelete.Delete();
+                adapter.Update(ds, "Туры");
+            }
+            */
+            reader.Close();
+        }
+
+        using (OleDbConnection connection = new OleDbConnection(connectionString))
+        {
+            connection.Open();
+
+            string selectQuery = "SELECT * FROM Туристы";
+            OleDbCommand selectCommand = new OleDbCommand(selectQuery, connection);
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(selectQuery, connection);
+            DataSet ds = new DataSet();
+            if (adapter != null && ds != null)
+            {
+                adapter.Fill(ds);
+                Console.WriteLine("не пустой");
+                // Другие операции с данными...
+            }
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                Console.WriteLine("не пустой");
+            }
+
+            DataRow newRow = ds.Tables["Table"].NewRow();
+            newRow[1] = "Стефанов";
+            newRow[2] = "Андрей";
+            newRow[3] = "Максимильянов";
+
+            ds.Tables["Table"].Rows.Add(newRow);
+
+            adapter.Update(ds, "Туристы");
+
+            DataRow userRow = ds.Tables["Table"].Rows.Find("Стефанов");
+            if (userRow != null)
+            {
+                userRow["Имя"] = "Максим";
+                adapter.Update(ds, "Туристы");
+            }
+        }
+    }
+}
---
+++ Zadanie_32/Zadanie_32/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+
+namespace Zadanie_32
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\SISDRUG\Desktop\Zadanie_32\Zadanie_32\Turisty.mdb;Persist Security Info=False;";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+
+
+                DataSet dataSet = new DataSet();
+
+
+                string toursQuery = "SELECT * FROM Туры";
+                string touristsQuery = "SELECT * FROM Туристы";
+
+
+                DataTable toursTable = new DataTable("Туры");
+                DataTable touristsTable = new DataTable("Туристы");
+
+
+                OleDbDataAdapter toursAdapter = new OleDbDataAdapter(toursQuery, connection);
+                toursAdapter.Fill(toursTable);
+
+                OleDbDataAdapter touristsAdapter = new OleDbDataAdapter(touristsQuery, connection);
+                touristsAdapter.Fill(touristsTable);
+
+                dataSet.Tables.Add(toursTable);
+                dataSet.Tables.Add(touristsTable);
+                //s
+
+
+                dataGridTours.ItemsSource = dataSet.Tables["Туры"].DefaultView;
+                dataGridTourists.ItemsSource = dataSet.Tables["Туристы"].DefaultView;
+
+
+                int tourCodeToDelete = 5;
+                string deleteTourQuery = "DELETE FROM Туры WHERE КодТура = 2";
+                OleDbCommand deleteTourCommand = new OleDbCommand(deleteTourQuery, connection);
+                deleteTourCommand.Parameters.AddWithValue("@TourCode", tourCodeToDelete);
+                int deletedRows = deleteTourCommand.ExecuteNonQuery();
+
+                Console.WriteLine("Удалено {0} записей", deletedRows);
+
+
+                int touristIdToUpdate = 1;
+                string newName = "Антон";
+                string updateTouristQuery = "UPDATE Туристы SET Имя = NewName WHERE TouristId = TouristId";
+                OleDbCommand updateTouristCommand = new OleDbCommand(updateTouristQuery, connection);
+                updateTouristCommand.Parameters.AddWithValue("NewName", newName);
+                updateTouristCommand.Parameters.AddWithValue("TouristId", touristIdToUpdate);
+                int updatedRows = updateTouristCommand.ExecuteNonQuery();
+
+                Console.WriteLine("Изменено {0} записей", updatedRows);
+            }
+        }
+    }
+}
